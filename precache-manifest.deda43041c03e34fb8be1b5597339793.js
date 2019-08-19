b3635a49346d506d17168108ae6049fca2eb39f1
Ankush Malik <ankushmalik631@gmail.com>
2019-08-19 09:32:27 +0530

    Updates

---
+++ precache-manifest.deda43041c03e34fb8be1b5597339793.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73142de20719121cc951adf13dc6fdab",
+    "url": "/Tetris/index.html"
+  },
+  {
+    "revision": "8733c0c6bc37eaea90fe",
+    "url": "/Tetris/static/css/main.32eb6a25.chunk.css"
+  },
+  {
+    "revision": "5ac36e04f684ac908b4e",
+    "url": "/Tetris/static/js/2.d83de239.chunk.js"
+  },
+  {
+    "revision": "8733c0c6bc37eaea90fe",
+    "url": "/Tetris/static/js/main.eada9087.chunk.js"
+  },
+  {
+    "revision": "0cafd22da1b85a6678c6",
+    "url": "/Tetris/static/js/runtime~main.9aec6341.js"
+  }
+]);
