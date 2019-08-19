d61abede91c46e9c141bc22b4681a2c3e293d069
Ankush Malik <ankushmalik631@gmail.com>
2019-08-19 11:57:38 +0530

    Add githubbanner

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { GamePage } from './pages/game/gamepage.component'
 
 const App = () => (
   <div className="App">
+    <a className='github-banner' href="https://github.com/AnkushMalik/Tetris">
+      <img src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub" />
+    </a>
     <h1>||| Tetris |||</h1>
     <GamePage />
   </div>
